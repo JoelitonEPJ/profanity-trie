3188b7980a40057d6b862aaed4f06ccc173faa84
Anderson Breno Santos Silva <anderson.breno.santos.silva@ccc.ufcg.edu.br>
2026-03-04 20:51:13 -0300

    Implementa a classe Regex (#3)
    * implementa a classe Regex
    
    * implementa buildPattern de Regex e complementa o arquivo de mapeamento de leets
    
    * deleta redundancias de codigo
    
    * conserta erro no compiler do pattern

--- src/main/java/regex/Regex.java
+++ src/main/java/regex/Regex.java
@@ -1,5 +1,61 @@
 package regex;
 
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Regex {
     
+    private Pattern pattern;
+
+    public Regex(String pattern) {
+        this.pattern = Pattern.compile(pattern, Pattern.CASE_INSENSITIVE);
+    }
+
+    public Regex(String[] palavras, HashMap<Character, String[]> leets) {
+        buildPattern(palavras, leets);
+    }
+
+    public void setPattern(String pattern) {
+        this.pattern = Pattern.compile(pattern);
+    }
+
+    public Matcher matcher(String entrada) {
+        return pattern.matcher(entrada);
+    }
+
+    public ArrayList<String> listarMatches(String entrada) {
+        ArrayList<String> matches = new ArrayList<>();
+
+        Matcher matcher = matcher(entrada);
+        while (matcher.find()) {
+            matches.add(matcher.group());
+        }
+
+        return matches;
+    }
+
+    public void buildPattern(String[] palavras, HashMap<Character, String[]> leetMap) {
+        StringBuilder pattern = new StringBuilder();
+
+        String padrao = "";
+        for (int i = 0; i < palavras.length; i++) {
+            char ultimoChar = 0;
+            String[] leets = {};
+            for (int j = 0; j < palavras[i].length(); j++) {
+                char charAtual = Character.toLowerCase(palavras[i].charAt(j));
+                leets = leetMap.get(charAtual);
+
+                if (ultimoChar != charAtual) padrao = "(?:" + String.join("|", leets) + ")+";
+
+                ultimoChar = charAtual;
+            }
+
+            if (!pattern.isEmpty()) pattern.append("|");
+            pattern.append(padrao); 
+        }
+
+        this.pattern = Pattern.compile(pattern.toString(), Pattern.CASE_INSENSITIVE);
+    }
 }
