4f055e22ae88f877043fa59fbdd5f934a5e2abb3
AndersonBreno1 <anderson.breno.santos.silva@ccc.ufcg.edu.br>
2026-03-04 20:23:15 -0300

    implementa buildPattern de Regex e complementa o arquivo de mapeamento de leets

--- src/main/java/regex/Regex.java
+++ src/main/java/regex/Regex.java
@@ -3,6 +3,8 @@ package regex;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.HashMap;
 
 public class Regex {
     
@@ -10,6 +12,10 @@ public class Regex {
 
     public Regex(String pattern) {
         this.pattern = Pattern.compile(pattern, Pattern.CASE_INSENSITIVE);
+    }
+
+    public Regex(String[] palavras, HashMap<Character, String[]> leets) {
+        buildPattern(palavras, leets);
     }
 
     public void setPattern(String pattern) {
@@ -29,5 +35,29 @@ public class Regex {
         }
 
         return matches;
+    }
+
+    public void buildPattern(String[] palavras, HashMap<Character, String[]> leetMap) {
+        StringBuilder pattern = new StringBuilder();
+
+        String padrao = "";
+        for (int i = 0; i < palavras.length; i++) {
+            padrao = "";
+            char ultimoChar = 0;
+            String[] leets = new String[0];
+            for (int j = 0; j < palavras[i].length(); j++) {
+                char charAtual = Character.toLowerCase(palavras[i].charAt(j));
+                leets = leetMap.get(charAtual);
+
+                if (ultimoChar != charAtual) padrao += "(?:" + charAtual + "|" + String.join("|", leets) + ")+";
+
+                ultimoChar = charAtual;
+            }
+
+            if (pattern.isEmpty()) pattern.append(padrao);
+            else                   pattern.append("|" + padrao); 
+        }
+
+        this.pattern = Pattern.compile(pattern.toString());
     }
 }
