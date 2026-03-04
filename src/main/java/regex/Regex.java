5037cdc819049c6bc50d9e9997e1d3ca8cc20c6b
AndersonBreno1 <anderson.breno.santos.silva@ccc.ufcg.edu.br>
2026-03-03 22:32:06 -0300

    implementa a classe Regex

--- src/main/java/regex/Regex.java
+++ src/main/java/regex/Regex.java
@@ -1,5 +1,35 @@
 package regex;
 
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import java.util.ArrayList;
+
 public class Regex {
     
+    private Pattern pattern;
+
+    public Regex(String pattern
+        
+    ) {
+        this.pattern = Pattern.compile(pattern, Pattern.CASE_INSENSITIVE);
+    }
+
+    public void setPattern(String pattern) {
+        this.pattern = Pattern.compile(pattern);
+    }
+
+    public Matcher matcher(String entrada) {
+        return pattern.matcher(entrada);
+    }
+
+    public ArrayList<String> listarMatches(String entrada) {
+        ArrayList<String> matches = new ArrayList<>();
+
+        Matcher matcher = matcher(entrada);
+        while (matcher.find()) {
+            matches.add(matcher.group());
+        }
+
+        return matches;
+    }
 }
