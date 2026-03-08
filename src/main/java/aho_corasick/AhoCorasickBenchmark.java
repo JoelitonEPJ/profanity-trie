47a19b0224fa0f209a93a24c43f69ac95eb138e6
arthurlazevedo <119319620+arthurlazevedo@users.noreply.github.com>
2026-03-08 17:49:20 -0300

    Implementa método genérico para rodar benchmark e adiciona script para rodá-la (#10)
    * melhora configuracao dos benchmarks e cria script configuravel para roda-los
    
    * atualiza baselinebenchmark e script
    
    * inicia configuracao dos benchmarks
    
    * simplifica benchmarks
    
    * cria maneira generica de rodar os testes e aplica para a hashtable como exemplo
    
    * formata codigo
    
    ---------
    
    Co-authored-by: arthur.ledra.ext <arthur.ledra.ext@synchro.com.br>

--- src/main/java/aho_corasick/AhoCorasickBenchmark.java
+++ src/main/java/aho_corasick/AhoCorasickBenchmark.java
@@ -1,49 +1,19 @@
 package aho_corasick;
 
-import java.util.concurrent.TimeUnit;
+import util.BenchmarkConfig;
 
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Fork;
-import org.openjdk.jmh.annotations.Level;
-import org.openjdk.jmh.annotations.Measurement;
-import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.OutputTimeUnit;
-import org.openjdk.jmh.annotations.Param;
-import org.openjdk.jmh.annotations.Scope;
-import org.openjdk.jmh.annotations.Setup;
-import org.openjdk.jmh.annotations.State;
-import org.openjdk.jmh.annotations.TearDown;
-import org.openjdk.jmh.annotations.Warmup;
-import org.openjdk.jmh.infra.Blackhole;
-                
-@State(Scope.Thread)
-@Fork(value = 5)
-@Warmup(iterations = 5, time = 5)
-@Measurement(iterations = 10, time = 5)
-@BenchmarkMode(Mode.AverageTime)
-@OutputTimeUnit(TimeUnit.NANOSECONDS)
-
-public class AhoCorasickBenchmark {
-    @Param({"1000", "5000", "10000"})
-    private int phraseSize;
-
-    private int correctAmount;
-
-    @Setup(Level.Trial)
-    public void setUp() {
+public class AhoCorasickBenchmark extends BenchmarkConfig {
+    @Override
+    public void addWords(String[] words) {
     }
 
-    @Benchmark
-    public void insertAll(Blackhole blackhole) {
+    @Override
+    public int countBadWords(String phrase) {
+        return 0;
     }
 
-    @Benchmark
-    public void searchPhrases(Blackhole blackhole) {
-    }
-
-    @TearDown(Level.Trial)
-    public void recordPhraseResults() {
+    @Override
+    public boolean checkIsBadWord(String word) {
+        return false;
     }
 }
-
