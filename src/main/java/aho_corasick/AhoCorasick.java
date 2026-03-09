a75004498d32958ab3042fa5d3a8bd48f7765e94
unknown <carlos.arthur.nobrega.soares@ccc.ufcg.edu.br>
2026-03-09 16:41:39 -0300

    implementação inicial

--- src/main/java/aho_corasick/AhoCorasick.java
+++ src/main/java/aho_corasick/AhoCorasick.java
@@ -1,5 +1,81 @@
 package aho_corasick;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
 public class AhoCorasick {
-    
+
+    static class Node {
+        Node[] next = new Node[26];
+        Node fail;
+        List<String> out = new ArrayList<>();
+    }
+
+    static Node root = new Node();
+
+    static void insert(String s) {
+        Node curr = root;
+
+        for (char c : s.toCharArray()) {
+            int idx = c - 'a';
+
+            if (curr.next[idx] == null)
+                curr.next[idx] = new Node();
+
+            curr = curr.next[idx];
+        }
+
+        curr.out.add(s);
+    }
+
+    static void build() {
+        Queue<Node> q = new ArrayDeque<>();
+
+        for (int i = 0; i < 26; i++) {
+            if (root.next[i] != null) {
+                root.next[i].fail = root;
+                q.add(root.next[i]);
+            }
+        }
+
+        while (!q.isEmpty()) {
+            Node curr = q.poll();
+
+            for (int i = 0; i < 26; i++) {
+                if (curr.next[i] != null) {
+                    Node f = curr.fail;
+
+                    while (f != null && f.next[i] == null)
+                        f = f.fail;
+
+                    curr.next[i].fail = (f == null) ? root : f.next[i];
+
+                    curr.next[i].out.addAll(curr.next[i].fail.out);
+
+                    q.add(curr.next[i]);
+                }
+            }
+        }
+    }
+
+    static void search(String text) {
+        Node curr = root;
+
+        for (int i = 0; i < text.length(); i++) {
+            int idx = text.charAt(i) - 'a';
+
+            while (curr != root && curr.next[idx] == null)
+                curr = curr.fail;
+
+            if (curr.next[idx] != null)
+                curr = curr.next[idx];
+
+            for (String s : curr.out)
+                System.out.println("\"" + s + "\" no índice: " + (i - s.length() + 1));
+        }
+    }
+
+    public static void main(String[] args) {}
 }
--- src/main/java/trie/Trie.java
+++ src/main/java/trie/Trie.java
@@ -4,12 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Trie {
-    private static class Node {
+    public static class Node {
         
         boolean fim;
         int contagem;    
         Map<Character,Node> filhos;    
-        
+
         public Node(){
             this.fim = false;
             this.contagem = 0;
@@ -19,31 +19,31 @@ public class Trie {
         
     private final Node raiz;
         
-        public Trie(){
-            this.raiz = new Node();
-        }
-    
-        public void insert(String texto){
-            Node atual = raiz;
-    
-            for (char caracter : texto.toCharArray()){
-                atual.filhos.putIfAbsent(caracter, new Node());
-    
-                atual = atual.filhos.get(caracter);
-                atual.contagem++;
-            }
-            atual.fim = true;
-        }
-    
-        public int query(String alvo){
-            Node atual = raiz;
-    
-            for (char caracter : alvo.toCharArray()){
-                atual = atual.filhos.get(caracter);
-    
-                if (atual == null) return 0;
-            }
-    
-            return atual.contagem;
-        }
+    public Trie(){
+        this.raiz = new Node();
     }
+
+    public void insert(String texto){
+        Node atual = raiz;
+
+        for (char caracter : texto.toCharArray()){
+            atual.filhos.putIfAbsent(caracter, new Node());
+
+            atual = atual.filhos.get(caracter);
+            atual.contagem++;
+        }
+        atual.fim = true;
+    }
+
+    public int query(String alvo){
+        Node atual = raiz;
+
+        for (char caracter : alvo.toCharArray()){
+            atual = atual.filhos.get(caracter);
+
+            if (atual == null) return 0;
+        }
+
+        return atual.contagem;
+    }
+}
