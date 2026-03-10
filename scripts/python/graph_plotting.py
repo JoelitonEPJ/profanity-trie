2fdf8753ed12742adc9c226c3a82dbe579c2b1f9
AndersonBreno1 <anderson.breno.santos.silva@ccc.ufcg.edu.br>
2026-03-10 14:54:01 -0300

    deleta arquivo extra acidental

--- scripts/python/graph_plotting.py
+++ scripts/python/graph_plotting.py
@@ -44,11 +44,7 @@ def insertion_perf_memory_usage():
         plt.ylabel("Uso de Memória (MB/op)", color="gray", fontsize=13)
         plt.title("Insert All Memory Usage Benchmark", fontsize=16)
         plt.grid(color="gray", axis="y", linestyle="dotted", alpha=0.3, zorder=1)
-        plt.savefig(GRAPHS_DIR/"insert_all_plot.png", dpi=300)
-        
-
-        # fig, ax = plt.subplots()
-
+        plt.savefig(GRAPHS_DIR/"insert_all.png", dpi=300)
 
 
 if __name__ == "__main__":
