7dc35802c3cf2048431adbd35717e0cfed63ab9f
gabrilito1 <gvictor.s.lima@gmail.com>
2026-03-09 22:14:24 -0300

    Melhora ProfanityTrie e adiciona métodos para o benchmark

--- src/main/java/trie/ProfanityTrie.java
+++ src/main/java/trie/ProfanityTrie.java
@@ -10,17 +10,19 @@ public class ProfanityTrie {
         this.root = new Node();
     }
 
-    public void addWords(String text, int index){
-            if (index == -1) {
-                index = text.length() - 1; 
-            }
+    public ProfanityTrie(String[] words){
+        this.root = new Node();
+        for (int i = 0; i < words.length; i++) addWords(words[i], -1);
+    }
 
+    public void addWords(String text, int index){
             Node current = root;
             int nodeCounting = 0;
     
             for (char caracter : text.toCharArray()){
                 current.childs.putIfAbsent(caracter, new Node());
                 current = current.childs.get(caracter);
+
                 current.counting++;
                 nodeCounting++;
 
@@ -28,34 +30,40 @@ public class ProfanityTrie {
                     current.isBadNode = true;
                 }
             }
+
             current.end = true;
         }
     
     public int countBadWords(String phrase){
             int count = 0;
             String[] words = phrase.split(" ");
+            
             for (String x : words){
                 if (checkIsBadWord(x)) count++;
             }
+
             return count;
     }
     
 
     public boolean checkIsBadWord(String alvo){
         Node current = root;
-    
+        
         for (char caracter : alvo.toCharArray()){
             current = current.childs.get(caracter);
+
             if (current == null) return false;
+
             if (current.isBadNode) return true;
         }
-            return false;
-        }
+
+        return current.end;
+    }
     private static class Node {
         boolean end;
         boolean isBadNode;
         int counting;
-        HashMap<Character,Node> childs = new HashMap<>();
+        HashMap<Character,Node> childs;
 
         public Node(){
             this.end = false;
--- src/main/java/trie/TrieBenchmark.java
+++ src/main/java/trie/TrieBenchmark.java
@@ -3,18 +3,21 @@ package trie;
 import util.BenchmarkConfig;
 
 public class TrieBenchmark extends BenchmarkConfig {
+    ProfanityTrie trie;
+
     @Override
     public void addWords(String[] words) {
+        trie = new ProfanityTrie(words);
     }
 
     @Override
     public int countBadWords(String phrase) {
-        return 0;
+        return trie.countBadWords(phrase);
     }
 
     @Override
     public boolean checkIsBadWord(String word) {
-        return false;
+        return trie.checkIsBadWord(word);
     }
 }
 
